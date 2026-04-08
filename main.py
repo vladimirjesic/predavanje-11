1b5289ce0f5378dc5cbddc522fc77eb78d07f1b2
vladimirjesic <vladimirjesicc@gmail.com>
2026-04-08 20:53:22 +0200

    Added expense_log.txt and logic for it in main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import json
 import sys
+from datetime import datetime
 
 user = None
 max_budget = 100000
 with open("data/user.json", "r") as file:
     user = json.load(file)
-user_budget = user['budget']
+user_budget = user['budget'] + user['credit']
+print(user_budget)
 if(user_budget < 0 or user_budget > max_budget):
     print("Budzet ne sme da bude manji od 0 ili veci od 100.000")
     sys.exit()
@@ -14,5 +16,10 @@ print(f"Dobar dan. Dobrodosli nazad, Vas budzet iznosi {user_budget}")
 
 expense = 0
 
-while(expense <= 0):
+while(expense <= 0 or expense>user_budget):
     expense = int(input("Unesite iznos Vaseg troska: "))
+
+with open("logs/expense_log.txt", "a") as file:
+    expense_log = None
+    expense_log = f"Amount: {expense}, User: {user['id']}, DateTime: {datetime.now()}, Budget: {user_budget}, Remaining budget: {user_budget-expense}\n"
+    file.write(expense_log)
